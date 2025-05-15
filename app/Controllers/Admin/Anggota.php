b1a8adf18f7f9c393e3b802a2d81acbfe4fa7531
lond01reng <kang.didikw@gmail.com>
2025-05-15 20:39:01 +0700

    Upload data siswa CSV

--- app/Controllers/Admin/Anggota.php
+++ app/Controllers/Admin/Anggota.php
@@ -56,8 +56,9 @@ class Anggota extends BaseController
       if($file = $this->request->getFile('f_csv')) {
         if ($file->isValid() && ! $file->hasMoved()) {
           $newName = $file->getRandomName();
-          $file->move('../public/csvfile', $newName);
-          $filePath = ("../public/csvfile/" . $newName);
+          $uploadPath = FCPATH . 'csvfile/';
+          $file->move($uploadPath, $newName);
+          $filePath = ($uploadPath . $newName);
 
           if($this->prosesCSV($filePath, $nota)===true){  
             return redirect()->to('admin/daftar_anggota/'.$nota)->with('success', 'Data berhasil diupload.');
